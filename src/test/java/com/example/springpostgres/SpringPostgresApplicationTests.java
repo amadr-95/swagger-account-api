01dbaf0b7b28c758b87f42d8589cb85198492209
Amador Sabido <asabido@viavansi.com>
2023-05-17 08:45:10 +0200

    minor changes

--- src/test/java/com/example/springpostgres/SpringPostgresApplicationTests.java
+++ src/test/java/com/example/springpostgres/SpringPostgresApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.springpostgres;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringPostgresApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
