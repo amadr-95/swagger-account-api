0fc2bc360b176bfdd0c1f2de781daecb7a5556b3
Amador Sabido <asabido@viavansi.com>
2023-05-25 09:44:10 +0200

    added swagger UI

---
+++ src/main/java/com/example/springpostgres/config/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.example.springpostgres.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI getOpenApi(){
+        /*Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.description("Server URL in Development environment");*/
+
+        Contact contact = new Contact();
+        contact.setEmail("asabido@viavansi.com");
+        contact.setName("Amador");
+
+        License mitLicense = new License().name("MIT License");
+
+        Info info = new Info()
+                .title("Account Management")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage user accounts")
+                .license(mitLicense);
+        return new OpenAPI().info(info);
+    }
+
+}
