8dcf4d1fe06c9fb1fef2e9af3fa82cad2f0650c4
Amador Sabido <asabido@viavansi.com>
2023-05-25 11:24:42 +0200

    added some swagger annotations

--- src/main/java/com/example/springpostgres/controller/AccountController.java
+++ src/main/java/com/example/springpostgres/controller/AccountController.java
@@ -2,6 +2,8 @@ package com.example.springpostgres.controller;
 
 import com.example.springpostgres.model.Account;
 import com.example.springpostgres.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@Tag(name = "Account", description = "Account management APIs")
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
@@ -21,6 +25,9 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(
+            summary = "Retrieve all Accounts or the filter ones by name"
+    )
     @GetMapping
     public ResponseEntity<List<Account>> findAll(
             @RequestParam(required = false, name = "name") String name){
@@ -28,35 +35,45 @@ public class AccountController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         return new ResponseEntity<>(accountService.findAll(name), HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Retrieve an Accounts by id"
+    )
     @GetMapping("/filter/id")
     public ResponseEntity<Optional<Account>> findById(@RequestParam Long id){
         if(accountService.findById(id).isEmpty())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(accountService.findById(id), HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Retrieve an Account by username"
+    )
     @GetMapping("/filter/username")
     public ResponseEntity<Optional<Account>> findByUsername(@RequestParam String username){
         if(accountService.findByUsername(username).isEmpty())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(accountService.findByUsername(username), HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Retrieve an Account by email"
+    )
     @GetMapping("/filter/email")
     public ResponseEntity<Optional<Account>> findByEmail(@RequestParam String email){
         if(accountService.findByEmail(email).isEmpty())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(accountService.findByEmail(email), HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Retrieve Accounts by email domain"
+    )
     @GetMapping("/filter/domain")
     public ResponseEntity<List<Account>> findByEmailContainsDomain(@RequestParam String domain){
         if(accountService.findByEmailContainsDomain(domain).isEmpty())
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         return new ResponseEntity<>(accountService.findByEmailContainsDomain(domain), HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Add a new Account"
+    )
     @PostMapping
     public ResponseEntity<HttpStatus> addAccount(@RequestBody Account account){
         try{
@@ -66,7 +83,9 @@ public class AccountController {
         }
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
+    @Operation(
+            summary = "Delete an Account by id"
+    )
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") Long id){
         try {
@@ -76,13 +95,17 @@ public class AccountController {
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Delete all Accounts"
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<HttpStatus> deleteAll(){
         accountService.deleteAll();
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
+    @Operation(
+            summary = "Update email and/or username from an Account"
+    )
     @PutMapping("/update/{id}")
     public ResponseEntity<HttpStatus> updateAccount(
             @PathVariable("id") Long id,
