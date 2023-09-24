3823b77975fb463ad4b13140e75490905291a1e4
Amador <amasabcar@outlook.com>
2023-09-24 22:10:32 +0200

    optimize imports

--- src/main/java/com/example/spring/account/Account.java
+++ src/main/java/com/example/spring/account/Account.java
@@ -3,7 +3,6 @@ package com.example.spring.account;
 
 import com.example.spring.customer.Customer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
